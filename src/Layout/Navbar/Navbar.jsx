36cbf294bbc94b762a39cf71333dbb14c1a785bf
Mujahid2000 <your.email@example.com>
2024-01-30 22:57:51 +0600

    add updated code

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -1,42 +1,48 @@
-import { FiShoppingCart } from "react-icons/fi";
-import './Navbar.css';
-import { useState } from "react";
-import { TbMenu } from "react-icons/tb";
-import { IoMdClose } from "react-icons/io";
+import { FiShoppingCart } from "react-icons/fi"
+import "./Navbar.css"
+import { useState } from "react"
+import { TbMenu } from "react-icons/tb"
+import { IoMdClose } from "react-icons/io"
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <nav className="main">
-            <button className="mobile-menu-icon" onClick={() => setIsOpen(!isOpen)}>
-                
-                {isOpen ? (
-                    <IoMdClose className="close"></IoMdClose>
-                ):(
-                    <TbMenu className="open"></TbMenu>
-                ) }
-            </button>
-            <div className="titleNavbar">
-                <img src="https://i.ibb.co/x1H40ZN/Taste-now.png" alt="" />
-            </div>
-            <div className="position">
-            <div className="navbar">
-                <ul className={isOpen? 'nav-link-mobile': 'route'} 
-                onClick={() => setIsOpen(false)}>
-                    <li className="home">Home</li>
-                    <li>About us</li>
-                    <li>Blog</li>
-                    <li>Contact us</li>
-                </ul>
-            </div>
-            
-            <div className="signButt">
-                    <FiShoppingCart className="Shopping" />
-                    <button className="signUp">Sign up</button>
-                </div>
-            </div>
-        </nav>
-    );
-};
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <nav className="main">
+      <button className="mobile-menu-icon" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? (
+          <IoMdClose className="close"></IoMdClose>
+        ) : (
+          <TbMenu className="open"></TbMenu>
+        )}
+      </button>
+      <div className="titleNavbar">
+        <img src="https://i.ibb.co/x1H40ZN/Taste-now.png" alt="" />
+      </div>
+      <div className="position">
+        <div className="navbar">
+          <ul
+            className={isOpen ? "nav-link-mobile" : "route"}
+            onClick={() => setIsOpen(false)}
+          >
+            <li className="home">Home</li>
+            <li>About us</li>
+            <li>Blog</li>
+            <li>Contact us</li>
+            {isOpen && (
+              <li>
+                <button className="signUp">Sign up</button>
+              </li>
+            )}
+          </ul>
+        </div>
 
-export default Navbar;
+        <div className="signButt">
+          <FiShoppingCart className="Shopping" />
+          {!isOpen && <button className="signUp">Sign up</button>}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
