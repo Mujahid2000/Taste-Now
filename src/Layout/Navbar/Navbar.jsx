13045a592dfe01d48827e2ff4aa4bbc01ff4539f
Mujahid2000 <your.email@example.com>
2024-02-03 22:10:41 +0600

    add some update code

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -5,16 +5,27 @@ import { TbMenu } from "react-icons/tb"
 import { IoMdClose } from "react-icons/io"
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [showSignUpButton, setShowSignUpButton] = useState(true);
+
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    if (window.innerWidth <= 640) {
+      setShowSignUpButton(!isOpen);
+    }
+  };
+  
   return (
     <nav className="main">
-      <button className="mobile-menu-icon" onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? (
-          <IoMdClose className="close"></IoMdClose>
-        ) : (
-          <TbMenu className="open"></TbMenu>
-        )}
-      </button>
+      <button className="mobile-menu-icon" onClick={toggleMenu}>
+  {isOpen ? (
+    <IoMdClose className="close"></IoMdClose>
+  ) : (
+    <TbMenu className="open"></TbMenu>
+  )}
+</button>
+
       <div className="titleNavbar">
         <img src="https://i.ibb.co/x1H40ZN/Taste-now.png" alt="" />
       </div>
@@ -37,9 +48,13 @@ const Navbar = () => {
         </div>
 
         <div className="signButt">
-          <FiShoppingCart className="Shopping" />
-          {!isOpen && <button className="signUp">Sign up</button>}
-        </div>
+  <FiShoppingCart className="Shopping" />
+  {!isOpen && showSignUpButton && window.innerWidth > 640 && (
+  <button className="signUp">Sign up</button>
+)}
+
+</div>
+
       </div>
     </nav>
   )
